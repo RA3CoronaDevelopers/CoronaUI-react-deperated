55ddcef189acb17abc52176767da43f6acab7e83
langyo <langyo.china@gmail.com>
2021-06-03 15:08:30 +0800

    📝 Add the css style matching section.

--- src/core/Box.tsx
+++ src/core/Box.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { css } from '@emotion/css';
+import React, { useRef, useEffect } from 'react';
+import { sjs, attr } from 'slow-json-stringify';
+import { generate } from 'shortid';
 
 type AroundBoxTemplate<T> = T | {
   all?: T,
@@ -100,41 +101,198 @@ type ITransformFunction = string | {
   }
 }
 
-export interface IBox {
-  componentType?: 'div' | 'span' | 'a' | 'p' | string,
-  styles?: {
-    border?: MediaQueryTemplate<AroundBoxTemplate<IBorder>>,
-    margin?: MediaQueryTemplate<AroundBoxTemplate<string | number>>,
-    padding?: MediaQueryTemplate<AroundBoxTemplate<string | number>>,
-    display?: MediaQueryTemplate<'inline' | 'block' | 'none'>,
-    overflow?: MediaQueryTemplate<'hidden' | 'visible' | 'scroll'>,
-    textOverflow?: MediaQueryTemplate<ITextOverflow | [ITextOverflow, ITextOverflow]>,
-    flex?: MediaQueryTemplate<IFlex>,
-    textAlign?: MediaQueryTemplate<ITextAlign | ITextAlign[]>
-    textColor?: MediaQueryTemplate<IColor>,
-    backgroundColor?: MediaQueryTemplate<IColor>,
-    zIndex?: 'up' | 'down' | string | number,
-    absolutePosition?: MediaQueryTemplate<AroundBoxTemplate<string | number>>,
-    shadow?: MediaQueryTemplate<number | 'none' | {
-      type?: 'box' | 'drop-shadow',
-      depth?: number,
-      color?: IColor
-    }>,
-    width?: MediaQueryTemplate<string | number>,
-    height?: MediaQueryTemplate<string | number>,
-    filter?: MediaQueryTemplate<IFilter>,
-    transform?: MediaQueryTemplate<ITransformFunction | string>
+interface IStyles {
+  border?: AroundBoxTemplate<IBorder>,
+  borderRadius?: string | number | (string | number)[],
+  margin?: AroundBoxTemplate<string | number>,
+  padding?: AroundBoxTemplate<string | number>,
+  display?: 'inline' | 'block' | 'none',
+  overflow?: 'hidden' | 'visible' | 'scroll',
+  textOverflow?: ITextOverflow | [ITextOverflow, ITextOverflow],
+  flex?: IFlex,
+  textAlign?: ITextAlign | ITextAlign[],
+  textColor?: IColor,
+  backgroundColor?: IColor,
+  zIndex?: 'up' | 'down' | string | number,
+  absolutePosition?: AroundBoxTemplate<string | number>,
+  shadow?: number | 'none' | {
+    type?: 'box' | 'drop-shadow',
+    depth?: number,
+    color?: IColor
   },
+  width?: string | number,
+  height?: string | number,
+  filter?: IFilter,
+  transform?: ITransformFunction | string,
   order?: number,
   flexGrow?: number,
   flexShrink?: number,
-  flexAlignSelf?: 'start' | 'end' | 'center',
+  flexAlignSelf?: 'start' | 'end' | 'center'
+}
+
+export interface IBox {
+  componentType?: 'div' | 'span' | 'a' | 'p' | string,
+  styles?: MediaQueryTemplate<IStyles>,
   children?: any
 }
 
-export function Box({ componentType, styles, children, ...props }: IBox) {
+const stringify = (() => {
+  function generateAroundBoxTemplateSchema(
+    schema: { [key: string]: any } | any,
+    expand?: boolean
+  ) {
+    const stringify = sjs({
+      ...(expand ? schema : {}),
+      all: schema,
+      row: schema,
+      column: schema,
+      top: schema,
+      bottom: schema,
+      left: schema,
+      right: schema
+    });
+
+    return attr('string', n => typeof n === 'object' ? stringify(n) : `${n}`);
+  }
+
+  const shadowSchemaStringify = sjs({
+    type: attr('string'),
+    depth: attr('number'),
+    color: attr('string', n => Array.isArray(n)
+      ? n.map(n => `${n}`).join(' ')
+      : `${n}`)
+  });
+
+  const martixSchemaStringify = sjs({
+    x: attr('string', n => `${n}`),
+    y: attr('string', n => `${n}`),
+    z: attr('string', n => `${n}`)
+  });
+
+  const transformFunctionSchemaStringify = sjs({
+    matrix: attr('array'),
+    matrix3d: attr('array'),
+    perspective: attr('number'),
+    rotate: attr('string', n => typeof n === 'object'
+      ? martixSchemaStringify(n)
+      : `${n}`),
+    rotate3d: attr('string', n => n?.map(n => `${n}`).join(' ')),
+    scale: attr('string', n => Array.isArray(n)
+      ? n.map(n => `${n}`).join(' ')
+      : n ? `${n}` : undefined),
+    scale3d: attr('array'),
+    skew: attr('string', n => Array.isArray(n)
+      ? n.map(n => `${n}`).join(' ')
+      : typeof n === 'object'
+        ? martixSchemaStringify(n)
+        : `${n}`),
+    translate: attr('string', n => Array.isArray(n)
+      ? n.map(n => `${n}`).join(' ')
+      : typeof n === 'object'
+        ? martixSchemaStringify(n)
+        : `${n}`),
+    translate3d: attr('string', n => Array.isArray(n)
+      ? n.map(n => `${n}`).join(' ')
+      : martixSchemaStringify(n)
+    )
+  });
+
+  const globalSchema = {
+    border: generateAroundBoxTemplateSchema({
+      width: attr('string', n => `${n}`),
+      style: attr('string'),
+      color: attr('string')
+    }, true),
+    borderRadius: attr('string', n => Array.isArray(n)
+      ? n.map(n => `${n}`).join(' ')
+      : `${n}`),
+    margin: generateAroundBoxTemplateSchema(attr('string', n => `${n}`)),
+    padding: generateAroundBoxTemplateSchema(attr('string', n => `${n}`)),
+    display: attr('string'),
+    overflow: attr('string'),
+    textOverflow: attr('string', n => Array.isArray(n)
+      ? n.map(n => `${n}`).join(' ')
+      : `${n}`),
+    flex: {
+      direction: attr('string'),
+      wrap: attr('boolean'),
+      reverse: attr('boolean'),
+      justifyContent: attr('string'),
+      alignItems: attr('string'),
+      alignContent: attr('string')
+    },
+    textAlign: attr('string', n => Array.isArray(n)
+      ? n.map(n => `${n}`).join(' ')
+      : `${n}`),
+    textColor: attr('string', n => Array.isArray(n)
+      ? n.map(n => `${n}`).join(' ')
+      : `${n}`),
+    backgroundColor: attr('string', n => Array.isArray(n)
+      ? n.map(n => `${n}`).join(' ')
+      : `${n}`),
+    zIndex: attr('string', n => `${n}`),
+    absolutePosition: generateAroundBoxTemplateSchema(attr('string', n => `${n}`)),
+    shadow: attr('string', n => typeof n === 'object'
+      ? shadowSchemaStringify(n)
+      : `${n}`),
+    width: attr('string', n => `${n}`),
+    height: attr('string', n => `${n}`),
+    filter: {
+      blur: attr('string', n => `${n}`),
+      brightness: attr('string', n => `${n}`),
+      contrast: attr('string', n => `${n}`),
+      dropShadow: attr('string', n => `${n}`),
+      grayScale: attr('string', n => `${n}`),
+      hueRotate: attr('string', n => `${n}`),
+      invert: attr('string', n => `${n}`),
+      opacity: attr('string', n => `${n}`),
+      saturate: attr('string', n => `${n}`),
+      sepia: attr('string', n => `${n}`)
+    },
+    transform: attr('string', n => typeof n === 'object'
+      ? transformFunctionSchemaStringify(n)
+      : n),
+    order: attr('number'),
+    flexGrow: attr('number'),
+    flexShrink: attr('number'),
+    flexAlignSelf: attr('string')
+  };
+
+  return sjs({
+    ...globalSchema,
+    xs: globalSchema,
+    sm: globalSchema,
+    md: globalSchema,
+    lg: globalSchema,
+    xm: globalSchema
+  });
+})();
+
+let cssIdMap: { [propsJsonStr: string]: string } = {};
+
+export function Box({ componentType, styles, children }: IBox) {
+  const style = useRef({});
+  useEffect(() => {
+    const key = stringify(styles);
+    cssIdMap = {
+      ...cssIdMap,
+      [key]: cssIdMap[key] || generate()
+    };
+
+    if (!document.querySelector(`#css-corona-${cssIdMap[key]}`)) {
+      const node = document.createElement('style');
+      node.id = `css-corona-${cssIdMap[key]}`;
+      const content = document.createTextNode(`
+        .corona-${cssIdMap[key]} {
+          ${''}
+        }
+      `)
+      node.appendChild(content);
+      document.head.appendChild(node);
+    }
+  });
+
   return React.createElement(componentType || 'div', {
-    className: css`
-    `
+    className: style.current
   }, children);
 }
